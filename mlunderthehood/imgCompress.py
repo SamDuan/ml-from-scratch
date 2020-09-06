d04692acbd157a1bf30076fc1d809836977867e3
Samuel Duan <hsduan@gmail.com>
2020-09-06 14:58:02 -0400

    Upload data and refactor code structure

--- mlunderthehood/imgCompress.py
+++ mlunderthehood/imgCompress.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-from clustering import clustering
+from mlunderthehood.clustering import clustering
 import time
 
 # set up the parameters prior to running the program
